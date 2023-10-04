c058d874cad90ede9629ffb6a1b25fa7b34f2ea4
unknown <grickoff@mail.ru>
2023-10-04 18:50:00 +0300

    start commit 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
 
 func main() {
 	fmt.Println("Life is hell")
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+
+	func initConfig() error {
+		viper.AddConfigPath("configs")
+		viper.SetConfigName("config")
+		return viper.ReadInConfig()
+	}
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/grickoff/todo
+
+go 1.19
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/spf13/viper v1.12.0
+)
+
+
